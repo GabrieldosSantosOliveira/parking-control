507c484e36a9b966d5be1f9e29a5995f69a11cbe
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-13 16:53:24 -0400

    Add swagger

---
+++ src/main/java/com/api/parkingcontrol/configs/SwaggerInterface.java
@@ -0,0 +1,7 @@
+package com.api.parkingcontrol.configs;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+public interface SwaggerInterface {
+  void addResourceHandlers(ResourceHandlerRegistry registry);
+}
