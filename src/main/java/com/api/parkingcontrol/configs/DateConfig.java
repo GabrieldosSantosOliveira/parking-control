c0ec8b36105d562905cf241bf4675548af38f9ee
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-13 16:51:53 -0400

    style: formatter code

--- src/main/java/com/api/parkingcontrol/configs/DateConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/DateConfig.java
@@ -1,6 +1,5 @@
 package com.api.parkingcontrol.configs;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
@@ -12,15 +11,16 @@ import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class DateConfig {
-    public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
-    public  static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER =new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+  public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+  public static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(
+      DateTimeFormatter.ofPattern(DATETIME_FORMAT));
 
-    @Bean
-    @Primary
-    public ObjectMapper objectMapper(){
-        JavaTimeModule module = new JavaTimeModule();
-        module.addSerializer(LOCAL_DATETIME_SERIALIZER);
-        return new ObjectMapper()
-                .registerModule(module);
-    }
+  @Bean
+  @Primary
+  public ObjectMapper objectMapper() {
+    JavaTimeModule module = new JavaTimeModule();
+    module.addSerializer(LOCAL_DATETIME_SERIALIZER);
+    return new ObjectMapper()
+        .registerModule(module);
+  }
 }
